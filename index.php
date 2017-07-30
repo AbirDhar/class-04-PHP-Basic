da922734a827eec04ebd75f39b26a901df0e403a
AbirDhar <rinky.raj.292@gmail.com>
2017-07-30 16:04:11 +0600

    class-04
    PHP-Basic

---
+++ index.php
@@ -0,0 +1,66 @@
+ <?php
+/*
+Class 01 :::::::::::: 
+*/
+    
+    echo 520; 
+    echo 'hello world';
+    print('new world');
+    echo 'line space kivabe dibo ??';
+/*
+Class 02 :::::::::::: 
+*/
+
+    
+$value_one = 'Hello from RRF Online Batch 2017';
+echo $value_one . '<br/>';
+
+$value_one = 'This is class 02';
+echo $value_one. '<br/>';
+
+$year = 2017;
+echo $year. '<br/>';
+/*
+Home Work class 02 :::::::::::: 
+*/
+
+$name = 'Lancer Abir';
+$address = '46, nobabganj road, lalbagh, dhaka 1211';
+$age = 27;
+
+echo $name, $address, $age. '<br/>';
+
+
+/*
+Class 03 :::::::::::: 
+*/
+
+define('NAME', 'Avash Kumar Dhar');
+define('BOARD', 'Dhaka board');
+define('GPA', 3.67);
+
+echo NAME, BOARD, GPA; 
+echo '<br/>';
+
+/*
+Class 04 :::::::::::: 
+*/
+
+$number1 = 100;
+$number2 = 25;
+
+echo $number1 + $number2;
+echo '<br/>';
+echo $number1 - $number2;
+echo '<br/>';
+echo $number1 * $number2;
+echo '<br/>';
+echo $number1 / $number2;
+echo '<br/>';
+echo $number1 % $number2;
+echo '<br/>';
+
+
+
+
+?>
